383f4b6cd5586799eebd37fae0a6d6204359a433
Vadim Salavatov <vmsalavatov@gmail.com>
2021-10-12 10:24:36 +0300

    +metaballs scene

--- src/main.cpp
+++ src/main.cpp
@@ -31,10 +31,44 @@ auto booba = [](float x, float y, float t) {
 };
 
 
+struct Meatball {
+    std::function<Vec2(float t)> position;
+    float c;
+    float r;
+};
+auto make_meatballs = [](std::vector<Meatball> spec) -> Plottable {
+    return [spec=std::move(spec)] (float x, float y, float t) -> float {
+        float result = 0;
+        for (auto& m : spec) {
+            Vec2 pos = m.position(t);
+            result += m.c * exp(- (sqr(x - pos.x) + sqr(y - pos.y)) / sqr(m.r));
+        }
+        return result;
+    };
+};
+
+auto meatballs = make_meatballs({
+    Meatball{
+        [](float t) { return Vec2{ 2.0f + sin(t), 3.0f + 2 * cos(t) + sqr(sin(t * 1.3f)) };},
+        -3.0,
+        5.0
+    },
+    Meatball{
+        [](float t) { return Vec2{ 4.f * sin(t * -0.3f) - 1.f, 1.1f + sin(t) };},
+        4.5,
+        2.4
+    },
+    Meatball{
+        [](float t) { return Vec2{ 1.f * sqr(cos(t * -5.f)) - 1.2f, 0.1f + 2.f * cos(t) - 0.3f * sin(t*0.7f) + 0.1f * sqr(sin(t)) };},
+        2.1,
+        10
+    },
+});
+
 int main() try {
 
     auto plotter = PlotterUI();
-    plotter.run(booba);
+    plotter.run(meatballs);
 
 } catch (std::exception const & e)
 {
